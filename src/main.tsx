b112ad22e0d44e2ac1605b14e6f9082092495f0a
Andy Santana <andysantana0106@gmail.com>
2024-05-15 19:29:09 -0400

    feat: add livesession and sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import * as Sentry from "@sentry/react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from "react-router-dom";
 import "./index.css";
 import { constructionRoutes } from "./modules/construction";
 import { RootLayout } from "./root-layout";
@@ -12,6 +20,24 @@ const router = createBrowserRouter([
     children: [constructionRoutes],
   },
 ]);
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
