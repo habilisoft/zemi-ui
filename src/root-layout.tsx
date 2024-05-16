7b29fd97fe1e877ed7648768b41d0d12ed34266c
Andy Santana <andysantana0106@gmail.com>
2024-05-15 20:52:26 -0400

    feat: add livesession

--- src/root-layout.tsx
+++ src/root-layout.tsx
@@ -1,8 +1,14 @@
 import { Outlet } from "react-router-dom";
+import ls from "@livesession/sdk";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
+import { useEffect } from "react";
 
 export const RootLayout = () => {
+  useEffect(() => {
+    ls.init(import.meta.env.VITE_LIVE_SESSION_CODE);
+    ls.newPageView();
+  }, []);
   return (
     <div className="flex flex-col h-screen">
       <Header />
